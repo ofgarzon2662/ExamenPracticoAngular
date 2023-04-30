06c30c633060d41304607d8bdfe73118d4b18691
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2023-04-29 20:18:04 -0700

    App Running. Bootstrap and tests to be perform

--- mynewapp/src/app/planta/planta.component.ts
+++ mynewapp/src/app/planta/planta.component.ts
@@ -13,14 +13,34 @@ export class PlantaComponent implements OnInit {
 
   constructor(private plantaService: PlantaService) { }
   plantas: Array<Planta> = [];
-
+  interior: number = 0;
+  exterior: number = 0;
+  total: number = 0;
   getPlants() {
     this.plantaService.getPlants().subscribe(plantas => {
       this.plantas = plantas;
     });
   }
+
+  loopData(): void {
+    for (let i = 0; i < this.plantas.length; i++) {
+      if (this.plantas[i].tipo === 'Interior') {
+        this.interior += 1;
+      }
+      if (this.plantas[i].tipo === 'Exterior') {
+        this.exterior += 1;
+      }
+    }
+  }
+
   ngOnInit() {
     this.getPlants();
+    this.plantaService.getPlants().subscribe(plantas => {
+      this.plantas = plantas;
+      this.loopData();
+    });
+
+
   }
 
 }
